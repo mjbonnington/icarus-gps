a843f2575bd3feae9fe664a6885344ff8daa1390
nunopereira <nuno.pereira@gps-ldn.com>
2014-10-02 19:20:38 +0100

    Updated job list

--- core/ui/jobs.py
+++ core/ui/jobs.py
@@ -7,18 +7,20 @@
 
 dic = {
 '110053_The_Louvre':'/Volumes/hggl_SAN_1/Project_Media/110053_The_Louvre/2009753_The_Louvre',
-'115388_Betfair_Worldcup':'/Volumes/hggl_SAN_1/Project_Media/115388_Betfair_Worldcup/2015552_Betfair_WC_Design_Pre-vis',
-'115388_Betfair_WC_Cashout': '/Volumes/hggl_SAN_1/Project_Media/115388_Betfair_Worldcup/2017089_Betfair_Cashout_30',
-'117997_Colgate_Gravity':'/Volumes/hggl_SAN_1/Project_Media/117997_Colgate_Gravity/2018026_Colgate_Gravity',
-'118156_Nestle_Moleson_TVC':'/Volumes/hggl_SAN_1/Project_Media/118156_Nestle_Moleson_TVC/2018111_Nestle_Moleson_PreVis',
-'118679_Dyson_Robot_360vision':'/Volumes/hggl_SAN_1/Project_Media/118679_Dyson_Robot_360vision/2018601_Dyson_X223_360eye',
 '119618_Dyson_N264':'/Volumes/hggl_SAN_1/Project_Media/119618_Dyson_N264/2019294_Dyson_N264',
-'607371_Schwartz_Flavour_Your_Way':'/Volumes/hggl_SAN_1/Project_Media/607371_Schwartz_Flavour_Your_Way/2009175_Schwartz_Flavour_Your_Way_UK_30',
+'119488_Homebase_Xmas':'/Volumes/hggl_SAN_1/Project_Media/119488_Homebase_Xmas/2019374_Hombase_Xmas_30s',
 'RnD':'/Volumes/hggl_SAN_1/RnD/rnd_job'
 }
 
 
 #archived jobs
+#'115388_Betfair_Worldcup':'/Volumes/hggl_SAN_1/Project_Media/115388_Betfair_Worldcup/2015552_Betfair_WC_Design_Pre-vis',
+#'115388_Betfair_WC_Cashout': '/Volumes/hggl_SAN_1/Project_Media/115388_Betfair_Worldcup/2017089_Betfair_Cashout_30',
+#'117997_Colgate_Gravity':'/Volumes/hggl_SAN_1/Project_Media/117997_Colgate_Gravity/2018026_Colgate_Gravity',
+#'118156_Nestle_Moleson_TVC':'/Volumes/hggl_SAN_1/Project_Media/118156_Nestle_Moleson_TVC/2018111_Nestle_Moleson_PreVis',
+#'118679_Dyson_Robot_360vision':'/Volumes/hggl_SAN_1/Project_Media/118679_Dyson_Robot_360vision/2018601_Dyson_X223_360eye',
+#'119618_Dyson_N264':'/Volumes/hggl_SAN_1/Project_Media/119618_Dyson_N264/2019294_Dyson_N264',
+#'607371_Schwartz_Flavour_Your_Way':'/Volumes/hggl_SAN_1/Project_Media/607371_Schwartz_Flavour_Your_Way/2009175_Schwartz_Flavour_Your_Way_UK_30',
 #'604695_Pantene_Hebe':'/Volumes/hggl_SAN_1/Project_Media/604695_Pantene_Hebe/2007169_Pantene_Hebe_BB_20',
 #'606061_Sensodyne_Mouthwash':'/Volumes/hggl_SAN_1/Project_Media/606061_Sensodyne_Mouthwash_TV_Tag/2007245_Sensodyne_Mouthwash_TV_Tag',
 #'606721_Sun_Bingo':'/Volumes/hggl_SAN_1/Project_Media/606721_News_uk_Sun_Bingo/2008159_Sun_Bingo_Animation_Test',
