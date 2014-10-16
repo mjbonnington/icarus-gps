35d9717c47674fe8e217002382f00ba2841ea280
nunopereira <nuno.pereira@gps-ldn.com>
2014-10-16 17:36:26 +0100

    Fixed broken paths in realflow commandsOrganizer.dat. Cleaned up realflow startup.py unused an loose code. Fixed pipeline internal path references in startup.rfs

--- realflow_rsc/scripts/startup.py
+++ realflow_rsc/scripts/startup.py
@@ -15,25 +15,22 @@ def autoDeploy():
 	#deploying commands organizer
 	outputMsg = 'Deploying GPS tools - '
 	try:
-		#deploying commands organizer
-		if not os.path.isdir(rf_userPath):
-			os.system('mkdir -p %s' % rf_userPath)
-		#copying the file to the user documents direcotry so users can overried shelf manually to workaournd realflow erratic loading behaviours
-		os.system('cp -f %s/%s %s/%s' % (rf_envPath, rf_cmdsOrg, rf_userPath, rf_cmdsOrg))
+		#deploying commands organizer copying the file to user scenes directory so it's not read directly from master file in pipeline
 		os.system('cp -f %s/%s %s/%s/%s' % (rf_envPath, rf_cmdsOrg, os.environ['REALFLOWSCENESDIR'], rf_cmdsOrgDir, rf_cmdsOrg))
 		print '%s Ok' % outputMsg 
 	except:
 		print '%s Failed' % outputMsg
 		
 def setupScene(scene):
-	#appending realflow pipeline paths to sys
-	sys.path.append(os.path.join(os.getenv('PIPELINE'), 'realflow_rsc', 'scripts'))
-	sys.path.append(os.path.join(os.getenv('PIPELINE'), 'realflow_rsc', 'realflow__env__'))
-	sys.path.append(os.path.join(os.getenv('PIPELINE'), 'icarus', 'ui'))
-	sys.path.append(os.path.join(os.getenv('PIPELINE'), 'icarus', 'libs', 'shared'))
-
-
-	#setting shot settings
+	#updating scene with shot settings
 	scene.setFps(int(os.environ['FPS']))
 	maxFrame = (int(os.environ['ENDFRAME']) - int(os.environ['STARTFRAME']))
 	scene.setMaxFrames(int(maxFrame))
+	
+
+	
+	
+	
+	
+	
+	
