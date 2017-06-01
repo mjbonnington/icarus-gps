fe57861e3eb21ff80821b81f6774d72ac5a88467
Mike Bonnington <mike.bonnington@gps-ldn.com>
2017-06-01 12:40:18 +0100

    Pre-merge

--- core/ui/p3.bat
+++ core/ui/p3.bat
@@ -1,3 +1,4 @@
 @echo off
-"C:\Program Files (x86)\Python36-32\python.exe" %1
+@"C:\Program Files (x86)\Python36-32\python.exe" %1
 @"C:\Users\mikebonnington\AppData\Local\Programs\Python\Python36-32\python.exe" %1
+"C:\Users\mikebonnington\AppData\Local\Programs\Python\Python36\python.exe" %1
