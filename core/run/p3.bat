6786929d26d452bbbe45415bf7a92cc8140a5637
Mike Bonnington <mike.bonnington@gps-ldn.com>
2017-06-30 10:14:01 +0100

    Fixed Windows Python3 launcher batch file

--- core/run/p3.bat
+++ core/run/p3.bat
@@ -1,4 +1,5 @@
 @echo off
-@"C:\Program Files (x86)\Python36-32\python.exe" %1
-@"C:\Users\mikebonnington\AppData\Local\Programs\Python\Python36-32\python.exe" %1
+REM "C:\Program Files (x86)\Python36-32\python.exe" %1
+REM "C:\Program Files\Python36\python.exe" %1
+REM "C:\Users\mikebonnington\AppData\Local\Programs\Python\Python36-32\python.exe" %1
 "C:\Users\mikebonnington\AppData\Local\Programs\Python\Python36\python.exe" %1
